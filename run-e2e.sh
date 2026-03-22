b04be48f7e8d4f7e1fbc69cce9c1b7cad54e80a4
Charles Dunn <charlesdunn0603@gmail.com>
2026-03-22 15:52:27 -0500

    h

--- run-e2e.sh
+++ run-e2e.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-export E2E_USER_EMAIL="charlesdunn2006@gmail.com"
-export E2E_USER_PASSWORD="Kgrace0603!"
-cd "$(dirname "$0")"
-npx playwright test --reporter=list --timeout=20000 "$@"
+cd /Users/charlesdunn/short-go-app
+export E2E_BASE_URL="https://short-go-app-git-dev-shortgoequinechiro-9122s-projects.vercel.app"
+npx playwright test e2e/full-app.spec.ts --grep "Public Pages" --reporter=list --project=chromium --timeout=15000 2>&1 | tail -20
